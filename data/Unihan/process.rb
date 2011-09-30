2e801ee533339b91d5e3a9022c20ab26f2d523db
Chen Yufei <cyfdecyf@gmail.com>
2011-09-30 15:29:59 +0800

    Add unihan codepoint to pinyin data.

---
+++ data/Unihan/process.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+# Runs with ruby 1.9
+
+File.open("./kMandarin.txt") do |f|
+  File.open('./unihan-codepoint2py.txt', 'w') do |out|
+    f.each_line do |line|
+      cp2py = line.chomp.split()
+      cp = cp2py[0]
+      py = cp2py[1, cp2py.size].map do |py|
+        py.downcase!
+        # remove tone
+        py = py[0, py.size - 1] if py[py.size - 1] =~ /\d/
+      end
+      py.uniq!
+
+      hz = [cp.to_i(16)].pack('U')
+      out.puts("#{cp} #{hz} #{py.join(' ')}")
+    end
+  end
+end
