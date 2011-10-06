dfacacfa436c091ee49efd82402ba80300827767
Chen Yufei <cyfdecyf@gmail.com>
2011-10-06 23:20:17 +0800

    block1 pinyin statistic to output all pinyins.
    The input data has only 1 pinyin for each Hanzi, also update for this.

--- data/Unihan/block1-pinyin-statistic.rb
+++ data/Unihan/block1-pinyin-statistic.rb
@@ -9,6 +9,7 @@ multipy_cnt = 0 # 3651
 # characters with less than or equal to 4 bytes pinyin
 le4py_cnt = 0 # 15736
 total_cnt = 0 # 20253
+longest_pinyin = 0
 
 File.open('./block1-codepoint2pinyin.txt') do |f|
   f.each_line do |line|
@@ -16,14 +17,17 @@ File.open('./block1-codepoint2pinyin.txt') do |f|
 
     cp = arr[0].to_i(16)
     py = arr[1, arr.size - 1]
+    longest_pinyin = py[0].size if py[0].size > longest_pinyin
 
     multipy_cnt += 1 if py.size > 1
+    # only consider the first pinyin
     le4py_cnt += 1 if py.size == 1 and py[0].size <= 4
     total_cnt += 1
 
     pyid = py_set[py]
     unless pyid
-      py_tbl << py
+      #py_tbl << py
+      py_tbl << py[0] # We have only one pinyin for each Hanzi in the input data
       py_set[py] = py_tbl.size
     end
   end
@@ -31,4 +35,7 @@ end
 
 puts "Number of multi-pinyin characters: #{multipy_cnt}"
 puts "Number of characters with less than or equal to 4 bytes pinyin: #{le4py_cnt}"
+puts "Longest pinyin: #{longest_pinyin}"
 puts "Total number of characters: #{total_cnt}"
+puts "Total number of pinyin: #{py_tbl.size}"
+p py_tbl.sort
