c5edbc87e95d17a232ab2dbe3db30a2d67a90b83
Chen Yufei <cyfdecyf@gmail.com>
2012-05-01 09:51:18 +0800

    Change cp2py to convert Chinese string to Pinyin.
    The original version accepts codepoint on the command line.

--- c/cp2py.c
+++ c/cp2py.c
@@ -5,25 +5,13 @@
 int main(int argc, char* argv[])
 {
     if (argc < 2) {
-        printf("Usage: %s <cp1> [cp2, cp3]\n", argv[0]);
+        printf("Usage: %s <Chiniese string>\n", argv[0]);
         exit(1);
     }
 
-    int i = 1;
-    uint32_t cp;
-    const char *pinyin;
-
-    for (; i < argc; i++) {
-        cp = (uint32_t)strtol(argv[i], NULL, 0);
-        pinyin = hz2pinyin_codepoint(cp);
-
-        if (i > 1)
-            printf(" ");
-        if (pinyin)
-            printf("%s", pinyin);
-        else if ((char)cp == cp) /* If it's ascii, print the letter. */
-            printf("%c", (char)cp);
-    }
+    char *pinyin = hz2pinyin(argv[1], " ", ENCODING_UTF8);
+    printf("%s\n", pinyin);
+    free(pinyin);
 
     return 0;
 }
